16cb20051d92503487d3a431628bf6191d1b61a1
NitikornSasomwong <88530715+NitikornSasomwong@users.noreply.github.com>
2021-08-20 15:09:50 +0700

    Add files via upload

---
+++ 2008_2.py
@@ -0,0 +1,4 @@
+width = float(input("Input the width: "))
+height = float(input("Input the height: "))
+Rectanglearea = width * height
+print("The Rectangle area is", Rectanglearea, "square units.")
